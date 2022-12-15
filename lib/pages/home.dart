68f4db36dfee8d8cf5d099563635d3d47fe2e694
aidandf29 <muhammad.aidan@ui.ac.id>
2022-12-15 21:30:15 +0700

    home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,15 @@
+import 'dart:js';
+
 import 'package:evote/components/vote_card.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:evote/pages/splash.dart';
+
+import '../components/dummy.dart';
 // import 'package:get/get.dart';
 
 class Home extends StatelessWidget {
+  List<Item> items = Item.generatedItem;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,9 +35,17 @@ class Home extends StatelessWidget {
               padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
               height: 330,
               child: Expanded(
-                child: ListView(
+                child: ListView.builder(
                   scrollDirection: Axis.horizontal,
-                  children: [VoteCard(), VoteCard(), VoteCard()],
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    if (index < items.length) {
+                      final item = items[index];
+                      return listItem(item);
+                    } else {
+                      return Text("X");
+                    }
+                  },
                 ),
               ),
             ),
@@ -53,9 +66,17 @@ class Home extends StatelessWidget {
               padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
               height: 330,
               child: Expanded(
-                child: ListView(
+                child: ListView.builder(
                   scrollDirection: Axis.horizontal,
-                  children: [VoteCard(), VoteCard(), VoteCard()],
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    if (index < items.length) {
+                      final item = items[index];
+                      return listItem(item);
+                    } else {
+                      return Text("X");
+                    }
+                  },
                 ),
               ),
             ),
@@ -68,5 +89,73 @@ class Home extends StatelessWidget {
                 })
           ]),
         )));
+  }
+
+  Widget listItem1(Item item) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+      height: 330,
+      child: Expanded(
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [VoteCard(), VoteCard(), VoteCard()],
+        ),
+      ),
+    );
+  }
+
+  Widget listItem(Item item) {
+    return Container(
+        padding: EdgeInsets.all(5),
+        child: Container(
+          width: 180,
+          decoration: BoxDecoration(
+            color: Color(0xFFE4E4E4),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsetsDirectional.only(top: 20)),
+              Container(
+                height: 135,
+                width: 135,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage('${item.imgUrl}'),
+                      fit: BoxFit.contain),
+                ),
+                child: Text(" "),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: Text(
+                  item.name.length < 15
+                      ? item.name
+                      : "${item.name.substring(0, 12)}..",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              LinearPercentIndicator(
+                lineHeight: 12,
+                percent: item.persentase / 100,
+                progressColor: Colors.green.shade300,
+                backgroundColor: Colors.grey,
+                barRadius: Radius.circular(5),
+                center: Text(
+                  '${item.persentase}%',
+                  style: TextStyle(color: Colors.white, fontSize: 11),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 15),
+                  height: 30,
+                  width: 100,
+                  decoration: BoxDecoration(color: Colors.red.shade200),
+                  child: Center(
+                    child: Text(item.waktu),
+                  ))
+            ],
+          ),
+        ));
   }
 }
