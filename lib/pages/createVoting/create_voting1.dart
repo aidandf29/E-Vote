0a6ecb4aef23053f7b4f9abb64aea5efd75ba876
adipratom <satriyo.adipratomo@ui.ac.id>
2022-12-21 02:25:54 +0700

    color correcting createvoting-adi

--- lib/pages/createVoting/create_voting1.dart
+++ lib/pages/createVoting/create_voting1.dart
@@ -374,48 +374,40 @@ class MyCustomFormState extends State<MyCustomForm> {
               // },
             ),
             Container(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                    padding: EdgeInsets.fromLTRB(300, 30, 0, 30),
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Validate returns true if the form is valid, or false otherwise.
-                          if (_formKey.currentState!.validate()) {
-                            // If the form is valid, display a snackbar. In the real world,
-                            // you'd often call a server or save the information in a database.
-                            // ScaffoldMessenger.of(context).showSnackBar(
-                            //   const SnackBar(content: Text('Processing Data')),
-                            // );
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //       builder: (context) => MultiForm()),
-                            // );
-                            create(namaController.text, now, _date,
-                                votersController.text);
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.symmetric(vertical: 20),
-                          primary: Color.fromARGB(1000, 255, 206, 64),
-                          onPrimary: Colors.white,
-                        ),
-                        child: Text(
-                          'Lanjutkan',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'Poppins',
-                            fontSize: 12,
-                          ),
+                child: Container(
+                    padding: EdgeInsets.fromLTRB(280, 30, 0, 30),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false otherwise.
+                        if (_formKey.currentState!.validate()) {
+                          // If the form is valid, display a snackbar. In the real world,
+                          // you'd often call a server or save the information in a database.
+                          // ScaffoldMessenger.of(context).showSnackBar(
+                          //   const SnackBar(content: Text('Processing Data')),
+                          // );
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //       builder: (context) => MultiForm()),
+                          // );
+                          create(namaController.text, now, _date,
+                              votersController.text);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        primary: Color.fromARGB(1000, 255, 206, 64),
+                        onPrimary: Colors.white,
+                      ),
+                      child: Text(
+                        'Lanjutkan',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Poppins',
+                          fontSize: 12,
                         ),
                       ),
-                    ))
-              ],
-            ))
+                    )))
           ],
         ),
       ),
